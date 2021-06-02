8e70b237762a2a588eb4c4b8856f22aef7963c7c
Rüegg Laura, B2B-BPN-HLT-DO-DEV <laura.rueegg@swisscom.com>
2021-06-02 20:02:35 +0200

    add controllers for every html page

--- docs/scripts/controllers/note-controller.js
+++ docs/scripts/controllers/note-controller.js
@@ -3,68 +3,21 @@ import { noteService } from '../services/note-service.js';
 /**
  * Click Event Listeners
  */
-if (document.querySelector('#sort-by-create-date-btn')) {
   document.querySelector('#sort-by-create-date-btn').addEventListener('click', () => {
     noteService.sortByCreatedDate();
     renderNotes();
   })
-}
-if (document.querySelector('#sort-by-importance-btn')) {
   document.querySelector('#sort-by-importance-btn').addEventListener('click', () => {
     noteService.sortByImportance();
     renderNotes();
   })
-}
-if (document.querySelector('#sort-by-duedate-btn')) {
   document.querySelector('#sort-by-duedate-btn').addEventListener('click', () => {
     noteService.sortByDueDate();
     renderNotes();
   })
-}
-if (document.querySelector('#cancel-note-creation-btn')) {
-  document.querySelector('#cancel-note-creation-btn').addEventListener('click', () => {
-    noteService.cancelNoteCreation();
-  })
-}
-if (document.querySelector('#show-finished-btn')) {
   document.querySelector('#show-finished-btn').addEventListener('click', () => {
     renderFinished();
   })
-}
-if (document.querySelector('#edit-note-save-btn')) {
-  document.querySelector('#edit-note-save-btn').addEventListener('click', () => {
-    noteService.cancelNoteCreation()
-  })
-}
-
-/**
- * Create Form
- */
-const formElement = document.querySelector('.form');
-if (formElement) {
-  // this logic is needed because otherwise the eventlistener would fire twice for input and label
-  let rating = document.getElementById('rate').addEventListener('click', event => {
-    const element = event.path[0];
-    if (element.tagName === 'INPUT') {
-      rating = element.value;
-    }
-  });
-  formElement.onsubmit = async (e) => {
-    e.preventDefault();
-    // initialize formData
-    const formData = new FormData();
-    const newNote = Object.fromEntries(formData);
-    newNote.title = document.querySelector('#title').value
-    newNote.description = document.querySelector('#description').value
-    newNote.importance = rating
-    newNote.created = moment().format().split('T')[0]
-    newNote.duedate = document.querySelector('#duedate').value
-    newNote.finished = false
-    noteService.createNote(newNote);
-    await noteService.loadData();
-    noteService.redirectToOverview();
-  };
-}
 
 /**
  * Note Template
@@ -144,8 +97,6 @@ function createNotesHTML(notes) {
           </div>
         </div>`).join('');
 }
-
-
 
 function renderFinished() {
   let todoCard = document.querySelectorAll('.todo-card')
