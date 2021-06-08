c3549bf0489f7bcf3279d79d159c771e573c3b71
Rüegg Laura, B2B-BPN-HLT-DO-DEV <laura.rueegg@swisscom.com>
2021-06-08 22:31:21 +0200

    finalize edit functionality

--- docs/scripts/controllers/note-edit-controller.js
+++ docs/scripts/controllers/note-edit-controller.js
@@ -1,15 +1,44 @@
 import { noteService } from '../services/note-service.js';
 
+let noteEdited = {
+    title: '',
+    description: '',
+    importance: '',
+    duedate: ''
+}
+
 /**
  * Click Event Listeners
  */
 document.querySelector('#cancel-note-creation-btn').addEventListener('click', () => {
     noteService.cancelRequest();
 })
-document.querySelector('#save-note-creation-btn').addEventListener('click', () => {
-    console.log('clicked save')
+document.querySelector('#save-note-creation-btn').addEventListener('click', async () => {
+    noteService.editNote(urlParams.id, noteEdited)
+    await noteService.loadData();
+    noteService.redirectToOverview();
 })
 
+/**
+ * Change Event Listeners
+ */
+document.querySelector('#title').addEventListener('change', (e) => {
+    noteEdited.title = e.target.value
+});
+document.querySelector('#description').addEventListener('change', (e) => {
+    noteEdited.description = e.target.value
+});
+document.querySelector('#rate').addEventListener('change', (e) => {
+    noteEdited.importance = e.target.value
+});
+document.querySelector('#duedate').addEventListener('change', (e) => {
+    noteEdited.duedate = e.target.value
+    console.log(noteEdited)
+});
+
+/**
+ * Prefill Form from URL Params
+ */
 const urlParams = Object.fromEntries(new URLSearchParams(window.location.search));
 document.querySelector('#title').setAttribute('value', urlParams.title);
 document.querySelector("textarea").value = urlParams.description;
